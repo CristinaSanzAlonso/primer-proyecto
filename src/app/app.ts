4a8f2fcfc674e2590058bc420f994878b9a5af2e
Cristina Sanz <cristina.sanz508@comunidadunir.net>
2026-02-14 18:42:45 +0100

    Comprobación de cambios en el deploy

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 //importamos el componente que hemos hecho -> home
 import { Home } from './home/home';
+import { PruebaComponent } from './components/prueba/prueba';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, PruebaComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
